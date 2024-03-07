073d00a314aeb89c5f392e245ff8a3462845f146
Lenah_Mbatha <lenahmbatha5@gmail.com>
2024-03-07 16:39:29 +0000

    myCommitToday

--- lib/widgets/custom_video_player.dart
+++ lib/widgets/custom_video_player.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:education_app/constants/icons.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
--- lib/widgets/forward_button.dart
+++ lib/widgets/forward_button.dart
@@ -1,16 +1,28 @@
-import 'package:flutter/cupertino.dart';
+// ignore_for_file: use_key_in_widget_constructors
+
 import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
 
-class ForwardButton extends StatefulWidget {
-  const ForwardButton({Key? key}) : super(key: key);
+class ForwardButton extends StatelessWidget {
+  final Function() onTap;
+  const ForwardButton({
+    Key? key,
+    required this.onTap,
+  });
 
-  @override
-  State<ForwardButton> createState() => _ForwardButton();
-}
-
-class _ForwardButton extends State<ForwardButton> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        width: 60,
+        height: 60,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(15),
+        ),
+        child: const Icon(Ionicons.chevron_forward_outline),
+      ),
+    );
   }
 }
