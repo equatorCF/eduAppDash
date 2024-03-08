db815738dca3ebb40a0588da57fed6417b44269f
Lenah_Mbatha <lenahmbatha5@gmail.com>
2024-03-08 09:05:46 +0000

    myCommitToday

--- lib/screens/account_screen.dart
+++ lib/screens/account_screen.dart
@@ -53,7 +53,8 @@ class _AccountScreenState extends State<AccountScreen> {
                 width: double.infinity,
                 child: Row(
                   children: [
-                    Image.asset("assets/avatar.png", width: 70, height: 70),
+                    Image.asset("assets/icons/avatar.png",
+                        width: 70, height: 70),
                     const SizedBox(width: 20),
                     const Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
