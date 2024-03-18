05b858ac463f0ec28498a49c8100227ebbcb1fbe
Lenah_Mbatha <lenahmbatha5@gmail.com>
2024-03-18 07:58:36 +0000

    myCommitToday

--- lib/models/category.dart
+++ lib/models/category.dart
@@ -35,6 +35,6 @@ List<Category> categoryList = [
   Category(
     name: 'Division',
     noOfCourses: 8,
-    thumbnail: 'assets/icons/su2.png',
+    thumbnail: 'assets/icons/lmk774.jpeg',
   ),
 ];
